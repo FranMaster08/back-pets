5820cac267b372fda602dcc124fe4f9f0da006fb
Francisco Javier Jimenez Cohen <franjicohen@gmail.com>
2024-10-04 18:49:12 -0500

    add : se agregan modulos de duenos y perros

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseModule } from './database/database.module';
+import { OwnerModule } from './owner/owner.module';
+import { DogsModule } from './dogs/dogs.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, OwnerModule, DogsModule],
   controllers: [],
   providers: [],
 })
